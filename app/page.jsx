e7c43aaa0cf486cb9e1f5f0f31ab8eb604e8e912
vasylmiskiv <vasylmiskiv@gmail.com>
2023-05-09 00:45:56 +0300

    init the project added global styles, defined layout and home page

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <section classNam="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Ai-powered prompts </span>
+      </h1>
+
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
